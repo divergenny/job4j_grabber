374a51291feab6242256f7eaa9035922c5437ce9
divergenny <divergenny@gmail.com>
2022-06-14 17:15:24 +0500

    2.2. Модель данных - Post. add link to equals and hashcode.

--- src/main/java/ru/job4j/grabber/models/Post.java
+++ src/main/java/ru/job4j/grabber/models/Post.java
@@ -15,12 +15,12 @@ public class Post {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Post post = (Post) o;
-        return id == post.id;
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(id, link);
     }
 
     @Override
